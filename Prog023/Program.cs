9b956e0b4d54ba3b30bf6177b70c0d2d77df4957
KseniTi <lioness2006@bk.ru>
2022-11-25 18:18:02 +0300

    Prog023

---
+++ Prog023/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+void Zadacha23()
+{
+
+    Console.WriteLine("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    
+    int index = 1;
+    
+    while(index <= number)
+    {
+        Console.WriteLine($"{index}^3 = {index * index * index}");
+        index++;
+    }
+
+}
+
+Zadacha23();

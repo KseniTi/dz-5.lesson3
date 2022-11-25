cfa20e3f559bc5f9c82c882445b28b722ced0699
KseniTi <lioness2006@bk.ru>
2022-11-25 18:05:02 +0300

    Prog019

---
+++ Prog019/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на
+вход пятизначное число и проверяет, является ли оно
+палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> да
+*/
+
+void Zadacha19()
+{
+    Console.WriteLine("Введите пятизначное число");
+    int number = Convert.ToInt32(Console.ReadLine());
+   
+    if(Convert.ToString(number)[0] == Convert.ToString(number)[4] && Convert.ToString(number)[1] == Convert.ToString(number)[3]) Console.WriteLine("Число палиндром");
+    else Console.WriteLine("Не является");
+}
+
+Zadacha19();
+
